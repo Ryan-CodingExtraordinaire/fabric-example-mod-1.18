b6c082d62b453ecec7568d97efc61e1387a3fbe9
Ryan Bright <sparta.bright@gmail.com>
2022-01-20 11:52:28 +1300

    starting to add 'items'

---
+++ src/main/java/net/ryan/firstmod/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.ryan.firstmod.item;
+
+import net.ryan.firstmod.RyansMod;
+
+public class ModItems {
+    public static void registerModItems() {
+        RyansMod.LOGGER.info("Registering Mod Items for"+RyansMod.MOD_ID);
+    }
+
+}
