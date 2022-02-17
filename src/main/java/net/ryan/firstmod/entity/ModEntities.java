bf2e53a8697a0e3f94a479151476bd115fd8a064
ryan <brighry@whs.school.nz>
2022-02-17 23:50:00 +1300

    EH, ok

--- src/main/java/net/ryan/firstmod/entity/ModEntities.java
+++ src/main/java/net/ryan/firstmod/entity/ModEntities.java
@@ -4,18 +4,16 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.ryan.firstmod.RyansMod;
 
 public class ModEntities {
-    public static final EntityType<ThrownSpearEntity> SPEAR_PROJECTILE = registerSpear(
-            "spear_projectile",
+    public static final EntityType<ThrownSpearEntity> SPEAR_PROJECTILE = Registry.register(
+            Registry.ENTITY_TYPE,
+            new Identifier(RyansMod.MOD_ID, "mod_projectile"),
             FabricEntityTypeBuilder.<ThrownSpearEntity>create(SpawnGroup.MISC, ThrownSpearEntity::new).build()
     );
-
-    public static <T extends PersistentProjectileEntity> EntityType<T> registerSpear(String name, EntityType<T> entity){
-        return Registry.register(Registry.ENTITY_TYPE, RyansMod.MOD_ID, entity);
-    }
 
       public static void registerModEntities()
     {
---
+++ src/main/java/net/ryan/firstmod/entity/SpearEntityRenderer.java
@@ -0,0 +1,52 @@
+package net.ryan.firstmod.entity;
+
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Matrix3f;
+import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Vec3f;
+
+public class SpearEntityRenderer extends EntityRenderer<ThrownSpearEntity> {
+    protected SpearEntityRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx);
+    }
+
+
+    private static final Identifier TEXTURE_LOCATION = new Identifier("resources.assets.firstmod.textures.iron_spear");
+    private static final RenderLayer RENDER_TYPE = RenderLayer.getEntityCutoutNoCull(TEXTURE_LOCATION);
+
+    private static void vertex(VertexConsumer vertexConsumer, Matrix4f matrix4f, Matrix3f matrix3f, int packedLight, float x, int y, int u, int v) {
+        vertexConsumer.vertex(matrix4f, x - 0.5F, (float)y - 1.0F, 0.0F).color(255, 255, 255, 255).texture((float)u, (float)v).overlay(OverlayTexture.DEFAULT_UV).light(packedLight).normal(matrix3f, 0.0F, 1.0F, 0.0F).next();
+    }
+    @Override
+    public void render(ThrownSpearEntity projectile, float x, float partialTick, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int packedLight) {
+        matrixStack.push();
+        matrixStack.scale(1F, 1F, 1F);
+        matrixStack.multiply(this.dispatcher.getRotation());
+        matrixStack.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180.0F));
+      //  matrixStack.multiply(Vec3f.POSITIVE_Y.getRadialQuaternion(projectile.age));
+        MatrixStack.Entry posestack$pose = matrixStack.peek();
+        Matrix4f matrix4f = posestack$pose.getPositionMatrix();
+        Matrix3f matrix3f = posestack$pose.getNormalMatrix();
+        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RENDER_TYPE);
+        vertex(vertexConsumer, matrix4f, matrix3f, packedLight, 0.0F, 0, 0, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, packedLight, 1.0F, 0, 1, 1);
+        vertex(vertexConsumer, matrix4f, matrix3f, packedLight, 1.0F, 1, 1, 0);
+        vertex(vertexConsumer, matrix4f, matrix3f, packedLight, 0.0F, 1, 0, 0);
+        matrixStack.pop();
+        super.render(projectile, x, partialTick, matrixStack, vertexConsumerProvider, packedLight);
+    }
+
+    @Override
+    public Identifier getTexture(ThrownSpearEntity entity) {
+        return  new Identifier("textures/block/diamond_block.png");
+    }
+
+
+}
